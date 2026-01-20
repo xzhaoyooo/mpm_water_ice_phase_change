f78e7baeb524852b3731d2b1e4debd5e27fc43fc
xzhaoyooo <x_zhaoyooo@outlook.com>
2026-01-20 17:58:26 +0100

    ...

--- _common/solvers/TaichiMatrixFreeCGSolverExample.py
+++ _common/solvers/TaichiMatrixFreeCGSolverExample.py
@@ -5,26 +5,30 @@ from taichi.linalg import MatrixFreeCG, LinearOperator
 ti.init(arch=ti.cpu)
 
 GRID = 2
-x = ti.field(dtype=ti.f32, shape=(GRID, GRID))
-b = ti.field(dtype=ti.f32, shape=(GRID, GRID))
+x = ti.field(dtype=ti.f32, shape=GRID * GRID)
+b = ti.field(dtype=ti.f32, shape=GRID * GRID)
+
+@ti.func
+def flatten(i: int, j: int) -> int:
+    return i * GRID + j
 
 @ti.kernel
 def init():
     for i, j in ti.ndrange(GRID, GRID):
         xl = i / (GRID - 1)
         yl = j / (GRID - 1)
-        b[i, j] = 1
-        x[i, j] = 0.0
+        b[flatten(i, j)] = 1
+        x[flatten(i, j)] = 0.0
 
 @ti.kernel
 def compute_Ax(v: ti.template(), mv: ti.template()): # pyright: ignore
-    for i, j in v:
-        l = v[i - 1, j] if i - 1 >= 0 else 0.0
-        r = v[i + 1, j] if i + 1 <= GRID - 1 else 0.0
-        t = v[i, j + 1] if j + 1 <= GRID - 1 else 0.0
-        b = v[i, j - 1] if j - 1 >= 0 else 0.0
+    for i, j in ti.ndrange(GRID, GRID):
+        l = v[flatten(i - 1, j)] if i - 1 >= 0 else 0.0
+        r = v[flatten(i + 1, j)] if i + 1 <= GRID - 1 else 0.0
+        t = v[flatten(i, j + 1)] if j + 1 <= GRID - 1 else 0.0
+        b = v[flatten(i, j - 1)] if j - 1 >= 0 else 0.0
         # Avoid ill-conditioned matrix A
-        mv[i, j] = 20 * v[i, j] - l - r - t - b
+        mv[flatten(i, j)] = 20 * v[flatten(i, j)] - l - r - t - b
 
 A = LinearOperator(compute_Ax)
 init()
